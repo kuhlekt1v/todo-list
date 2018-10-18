ee4a997b26a5065a5519591fe7da4af6957ccc01
Cody S <sheridan.codyj@gmail.com>
2018-10-18 06:16:28 -0400

    Functionality complete/styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check of specific todos by clicking
-$('li').on('click', function(){
+$('ul').on('click', 'li', function(){
   $(this).toggleClass('completed');
 });
 
 // Remove item when X is clicked
-$('span').on('click', function(e){
+$('ul').on('click', 'span', function(e){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+  if(e.which === 13){
+    // Grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val('');
+    // Create a new li and add to ul
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+  }
 });
